Return connected graph after hiding edges Correct precision computation Hide same percentage of links per node Zero index vertex labels in edge lists Use dot product for similarity

src/results.py
@@ -9,28 +9,39 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Generate link predicition results.")
 
     parser.add_argument('--embedding', nargs='?', default='arxiv_cleaned',
-                        help='Path to embedding. File type must be .npy')
+                        help='Embedding file. File type must be .npy')
+
+    parser.add_argument('--graph', nargs='?', default='arxiv_cleaned',
+                        help='Adjacency matrix file. File type must be .npy')
 
     parser.add_argument('--hidden-links', nargs='?', default='arxiv_cleaned',
-                        help='Path to hidden links. File type must be .npy')
+                        help='Hidden links file. File type must be .npy')
 
     return parser.parse_args()
 
 
-def generate_results(args):
-    emb = np.load('%s/../emb/%s_emb_iter_1_p_1_q_1_walk_40_win_10.npy' % (c, args.embedding))
+def generate_results(args, ignore_last=False):
+    emb = np.load('%s/../emb/%s.npy' % (c, args.embedding))
+    if ignore_last:
+        m = len(emb)-1
+        emb = emb[:m]
+
+    sim_scores = similarity_scores(emb, method='dot')
+    graph = np.load('%s/../graph/%s_hidden.npy' % (c, args.graph))
     hidden_links = np.load('%s/../graph/%s_testlinks.npy' % (c, args.hidden_links))
 
-    l2_sim_scores = similarity_scores(emb, method='euclidean')
-    cos_sim_scores = similarity_scores(emb, method='cosine')
+    for row in hidden_links:
+        x = row[0]
+        y = row[1]
+        graph[x][y] = graph[y][x] = 1
+    np.fill_diagonal(graph, 0)
 
     k_ = [2, 10, 100, 200, 300, 500, 800, 1000, 10000]
+    precisions = [precision(k, sim_scores, graph) for k in k_]
+    str_precisions = [("%f" % x) for x in precisions]
+    print '\t'.join(str_precisions)
 
-    for k in k_:
-        print 'l2 precision@k=%d: %f' % (k, precision(k, l2_sim_scores, hidden_links))
-
-    for k in k_:
-        print 'cos precision@k=%d: %f' % (k, precision(k, cos_sim_scores, hidden_links))
+    return
 
 
 if __name__ == "__main__":

src/utils.py
@@ -1,27 +1,34 @@
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
 
-
-def convert_embed_to_np(emb_file, np_file):
+def convert_embed_to_np(emb_file, np_file, ignore_last=False):
     print 'Convert %s to %s' % (emb_file, np_file)
     with open(emb_file) as f:
         lines = f.readlines()
     t = lines[0].split()
-    m = int(t[0])
+    s = m = int(t[0])
     n = int(t[1])
-    mat = np.zeros((m, n))
+
+    if ignore_last:
+        s = m-1
+
+    mat = np.zeros((s, n))
     for line in lines[1:]:
         t = line.rstrip().split()
         r = int(t[0])
+
         li = [ float(x) for x in t[1:] ]
-        mat[r] = li
+
+        if r != s or not ignore_last:
+            mat[r] = li
+
     np.save(np_file, mat)
 
 
 def format_edgelist(edg_file):
     '''
     Converts an edgelist with arbitrarily labelled vertices
-    to edgelist with vertices labeled 1...<num_vertices>
+    to edgelist with vertices labeled 0...<num_vertices-1>
     '''
     with open(edg_file) as f:
         lines = f.readlines()
@@ -35,10 +42,10 @@ def format_edgelist(edg_file):
     vertices = np.unique(temp)
 
     # create mapping of original vertex
-    # labels to 1...<num_vertices>
+    # labels to 0...<num_vertices-1>
     mapping = {}
     for x in range(len(vertices)):
-        mapping[vertices[x]] = x+1
+        mapping[vertices[x]] = x
 
     # convert edgelist with new labels
     outfile = '%s_cleaned.edgelist' % edg_file.split('.')[0]
@@ -64,12 +71,14 @@ def similarity_scores(emb, method):
         dist_mat = squareform(dist_array)
         sim_mat = 1 - dist_mat
         return sim_mat
+    elif method=='dot':
+        return np.dot(emb, emb.T)
     else:
         print "ERROR: Did not recognize method name. Use 'euclidean' or 'cosine'."
         raise SystemExit
 
 
-def precision(k, sim_scores, hidden_links):
+def precision_predicted(k, sim_scores, hidden_links):
     '''
     Returns the precision at k for predicted links
     given a matrix of similarity scores and array of hidden links.
@@ -78,8 +87,8 @@ def precision(k, sim_scores, hidden_links):
     n = len(sim_scores)
     mask = np.zeros((n, n))
     for row in hidden_links:
-        x = row[0] - 1
-        y = row[1] - 1
+        x = row[0]
+        y = row[1]
         mask[x][y] = mask[y][x] = 1
     np.fill_diagonal(mask, 0)
 
@@ -92,8 +101,32 @@ def precision(k, sim_scores, hidden_links):
         truncation = sorted_v[i][:k]
         true_labels = np.nonzero(mask[i])[0]
         intersection = np.intersect1d(truncation, true_labels, assume_unique=True)
+        # print intersection
         precision_i = float(len(intersection)) / k
         precision += precision_i
 
     m = np.count_nonzero(np.any(mask, axis=1))
     return precision / m
+
+
+def precision(k, sim_scores, graph):
+    '''
+    Returns the precision at k given original graph.
+    '''
+
+    n = len(graph)
+    # sort sim_scores along row axis and obtain indices
+    np.fill_diagonal(sim_scores, 0)
+    sorted_v = np.argsort(-sim_scores, axis=1)
+
+    precision = 0.0
+    for i in range(n):
+        truncation = sorted_v[i][:k]
+        true_labels = np.nonzero(graph[i])[0]
+        intersection = np.intersect1d(truncation, true_labels, assume_unique=True)
+        precision_i = float(len(intersection)) / k
+        precision += precision_i
+
+    m = np.count_nonzero(np.any(graph, axis=1))
+    return precision / m
+